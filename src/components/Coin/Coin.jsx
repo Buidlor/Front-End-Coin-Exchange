3cebc4a764793dfd21cfa3c74ff1828396d478a2
Buidlor <89695544+Buidlor@users.noreply.github.com>
2022-04-07 21:01:10 +0200

    git course

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -3,12 +3,50 @@ import './Coin.css'
 import PropTypes from 'prop-types';
 
 export default class Coin extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      price: this.props.price
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+ /* componentDidMount(){
+    const callback =() => {
+      //ste the state to a new random value
+      const randomPercentage = 0.995 + Math.random() * 0.01;
+      //don't do this: 
+      //this.state.price= this.state.price*randomPercentage;
+
+      this.setState(function(oldState) {
+        return {
+          price: oldState.price * randomPercentage
+        };
+      });
+    }
+    //setInterval(callback, 1000);
+  }*/
+
+  handleClick(event){
+    event.preventDefault();
+    const randomPercentage = 0.995 + Math.random() * 0.01;
+    this.setState(function(oldState) {
+      return {
+        price: oldState.price * randomPercentage
+      };
+    });
+  }
+
   render() {
     return (
       <tr className='coin-row'>
         <td>{this.props.name}</td>
         <td>{this.props.ticker}</td>
-        <td>${this.props.price}</td>
+        <td>${this.state.price}</td>
+        <td>
+          <form action ="#" method ="POST">
+            <button onClick={this.handleClick}>Refresh</button>
+          </form>
+        </td>
       </tr>
     );
   }
