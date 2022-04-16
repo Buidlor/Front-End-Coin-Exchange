ff46f63b76db27e48841f397a192848a9bc5072f
Buidlor <89695544+Buidlor@users.noreply.github.com>
2022-04-16 14:14:11 +0200

    Top 10 coins with 0 balance and price

--- src/App.js
+++ src/App.js
@@ -3,49 +3,37 @@ import Coinlist from './components/Coinlist/Coinlist';
 import AccountBalance from'./components/AccountBalance/AccountBalance';
 import Header from './components/Header/Header';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Div = styled.div ` 
   text-align: center;
   background-color: rgb(83, 77, 155);
   color: #61dafb;
 `
+
+const COIN_COUNT = 10;
+
 class App extends React.Component {
   state = {
     balance: 10000,
     showBalance: true,
-    coinData: [
-      {
-        name: 'Bitcoin',
-        ticker: 'BTC',
-        price: 40000,
-        balance: 1
-      },
-      {
-        name: 'Ethereum',
-        ticker: 'ETH',
-        price: 3000,
-        balance: 32
-      },
-      {
-        name: 'Terra',
-        ticker: 'LUNA',
-        price: 101,
-        balance: 200
-      },
-      {
-        name: 'Solana',
-        ticker: 'SOL',
-        price: 151,
-        balance: 0
-      },
-      {
-        name: 'Avalanche',
-        ticker: 'AVAX',
-        price: 85,
-        balance: 30
-      }
-    ]
+    coinData: []
   }
+componentDidMount = async () => {
+  let response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false')  
+  let coinData = response.data.slice(0, COIN_COUNT).map(function(coin){
+    return { 
+      key: coin.id,
+      name: coin.name,
+      ticker: coin.symbol,
+      balance: 0,
+      price: coin.current_price,
+    };
+  });
+  this.setState({coinData});
+     
+}
+
   handleBalanceVisibilityChange = () => {
     this.setState( function(oldState) {
       return {
